b453408b51f80d3f8e761f14c594d31fb3ba1c14
Alex Liew <liewtwoo42@gmail.com>
2023-10-10 12:14:37 -0400

    add some header css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,16 @@
 import './index.css'
 
 export function Header () {
+
+  function handleClick(e) {
+    e.preventDefault()
+    console.log('hea')
+  }
   return (
       <div className="header">
         <a className='logo'>ShopNow</a>
-        <div className="header-right">
-        </div>
+        <button onClick={handleClick} className="header-right">
+        </button>
       </div>
   )
 }
